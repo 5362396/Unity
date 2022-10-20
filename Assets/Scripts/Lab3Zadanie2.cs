ab1129c0fbe6ec9c603b3c8acf534a742c876f2c
5362396 <56536893+5362396@users.noreply.github.com>
2022-10-20 13:11:56 +0200

    Lab3 Zadanie2

---
+++ Assets/Scripts/Lab3Zadanie2.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lab3Zadanie2 : MonoBehaviour
+{
+    public float speed = 2.0f;
+    Rigidbody rb;
+    private float startPosition;
+    private int direction;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startPosition = transform.position.x;
+        direction = 1;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 velocity = new Vector3(1, 0, 0);
+        velocity = velocity.normalized * speed * Time.deltaTime;
+        Debug.Log(transform.position.x);
+        if (transform.position.x - startPosition < 10 && direction == 1)
+        {
+            rb.MovePosition(transform.position + velocity);
+        }
+        else if (transform.position.x - startPosition >= 10 && direction == 1)
+        {
+            direction = 0;
+        }
+        else if (transform.position.x - startPosition > 0 && direction == 0)
+        {
+            rb.MovePosition(transform.position - velocity);
+        }
+        else
+        {
+            direction = 1;
+        }
+    }
+}
