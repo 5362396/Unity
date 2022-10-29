1d24788b2f73ae5f8c9f3dca879f4ee1e4472460
5362396 <56536893+5362396@users.noreply.github.com>
2022-10-29 19:23:45 +0200

    Lab4 Zadanie4

--- Assets/Scripts/Lab4Zadanie4.cs
+++ Assets/Scripts/Lab4Zadanie4.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lab4Zadanie2 : MonoBehaviour
+public class Lab4Zadanie4 : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
+    private float playerSpeed = 5.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
+    public Transform player;
+    public float sensitivity = 2f;
+    private float mouseXMove = 0.0f;
+    private float mouseYMove = 0.0f;
 
     private void Start()
     {
@@ -17,23 +21,22 @@ public class Lab4Zadanie2 : MonoBehaviour
         controller.slopeLimit = 50.0f;
         controller.stepOffset = 0.5f;
         controller.minMoveDistance = 0f;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
         controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
 
         if (Input.GetButtonDown("Jump") && groundedPlayer)
         {
@@ -42,6 +45,12 @@ public class Lab4Zadanie2 : MonoBehaviour
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+
+        mouseXMove += Input.GetAxis("Mouse X") * sensitivity;
+        mouseYMove -= Input.GetAxis("Mouse Y") * sensitivity;
+        mouseYMove = Mathf.Clamp(mouseYMove, -90f, 90f);
+
+        transform.eulerAngles = new Vector3(mouseYMove, mouseXMove, 0);
     }
 
     void FixedUpdate()
