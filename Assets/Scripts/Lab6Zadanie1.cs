8c35ecb8fdd8f2b7a743db2a93e1dc7ee9c849cb
5362396 <56536893+5362396@users.noreply.github.com>
2022-11-10 18:22:30 +0100

    Lab6 Zadanie1

---
+++ Assets/Scripts/Lab6Zadanie1.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lab6Zadanie1 : MonoBehaviour
+{
+    public float elevatorSpeed = 2f;
+    private bool isRunning = false;
+    public float distance = 6.5f;
+    private bool isRunningUp = true;
+    private bool isRunningDown = false;
+    private float downPosition;
+    private float upPosition;
+    private Transform oldParent;
+
+    void Start()
+    {
+        upPosition = transform.position.x + distance;
+        downPosition = transform.position.x;
+    }
+
+    void Update()
+    {
+        if (isRunningUp && transform.position.x >= upPosition)
+        {
+            isRunning = false;
+        }
+        else if (isRunningDown && transform.position.x <= downPosition)
+        {
+            isRunning = false;
+        }
+
+        if (isRunning)
+        {
+            Vector3 move = transform.right * elevatorSpeed * Time.deltaTime;
+            transform.Translate(move);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player wszedł na windę.");
+            oldParent = other.gameObject.transform.parent;
+            other.gameObject.transform.parent = transform;
+            if (transform.position.x >= upPosition)
+            {
+                isRunningDown = true;
+                isRunningUp = false;
+                elevatorSpeed = -elevatorSpeed;
+            }
+            else if (transform.position.x <= downPosition)
+            {
+                isRunningUp = true;
+                isRunningDown = false;
+                elevatorSpeed = Mathf.Abs(elevatorSpeed);
+            }
+            isRunning = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player zszedł z windy.");
+            other.gameObject.transform.parent = oldParent;
+        }
+    }
+}
